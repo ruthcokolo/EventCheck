933f5145dcbb37beb6361d623e142f2498552673
Ruth Okolo <ruthcokolo@outlook.com>
2026-01-08 21:26:06 -0500

    Initial Commit

---
+++ EventCheck/EventCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventCheckApp.swift
+//  EventCheck
+//
+//  Created by Ruth Okolo on 2026-01-08.
+//
+
+import SwiftUI
+
+@main
+struct EventCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
